15ad21aa80f4c9c3263ad9503c06c2e7acf4144f
prihatnym <prihatnymichael@gmail.com>
2025-11-27 13:39:57 +0100

    Oprava navbar mobile dropdown - vertikálne menu s pekným štýlom

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,24 +6,39 @@ import './Navbar.css';
 export default function Navbar() {
   const [expanded, setExpanded] = useState(false);
 
+  const handleNavClick = () => {
+    setExpanded(false);
+  };
+
   return (
-    <BSNavbar expanded={expanded} onToggle={setExpanded} sticky="top" className="minecraft-navbar">
-      <Container>
-        <BSNavbar.Brand as={Link} to="/" className="navbar-brand-custom">
-          <i className="fas fa-cube" style={{ marginRight: '10px', color: 'var(--mc-bright-green)' }}></i>
+    <BSNavbar 
+      expand="lg" 
+      expanded={expanded} 
+      onToggle={setExpanded} 
+      fixed="top" 
+      className="minecraft-navbar"
+    >
+      <Container fluid>
+        <BSNavbar.Brand as={Link} to="/" className="navbar-brand-custom" onClick={handleNavClick}>
+          <i className="fas fa-cube"></i>
           <span className="brand-text">SPŠE Minecraft SMP</span>
         </BSNavbar.Brand>
-        <BSNavbar.Toggle aria-controls="basic-navbar-nav" />
-        <BSNavbar.Collapse id="basic-navbar-nav">
+        <BSNavbar.Toggle aria-controls="navbar-nav" className="border-0">
+          <span className="navbar-toggler-icon"></span>
+        </BSNavbar.Toggle>
+        <BSNavbar.Collapse id="navbar-nav">
           <Nav className="ms-auto nav-items">
-            <Nav.Link as={Link} to="/" className="nav-link-custom" onClick={() => setExpanded(false)}>
-              <i className="fas fa-home"></i> Domov
+            <Nav.Link as={Link} to="/" className="nav-link-custom" onClick={handleNavClick}>
+              <i className="fas fa-home"></i>
+              <span>Domov</span>
             </Nav.Link>
-            <Nav.Link as={Link} to="/rules" className="nav-link-custom" onClick={() => setExpanded(false)}>
-              <i className="fas fa-scroll"></i> Pravidlá
+            <Nav.Link as={Link} to="/rules" className="nav-link-custom" onClick={handleNavClick}>
+              <i className="fas fa-scroll"></i>
+              <span>Pravidlá</span>
             </Nav.Link>
-            <Nav.Link as={Link} to="/admin" className="nav-link-custom nav-link-admin" onClick={() => setExpanded(false)}>
-              <i className="fas fa-lock"></i> Admin
+            <Nav.Link as={Link} to="/admin" className="nav-link-custom nav-link-admin" onClick={handleNavClick}>
+              <i className="fas fa-lock"></i>
+              <span>Admin</span>
             </Nav.Link>
           </Nav>
         </BSNavbar.Collapse>
